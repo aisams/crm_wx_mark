0861be7e7a2543b23f9162bc990d6ad8f83deb51
zhuangjun.liao <zhuangjun.liao@591hx.com>
2018-04-24 11:35:14 +0800

    修改提交

--- src/com/crm/finance/MainActivity.java
+++ src/com/crm/finance/MainActivity.java
@@ -1,21 +1,3 @@
-/*
-       Licensed to the Apache Software Foundation (ASF) under one
-       or more contributor license agreements.  See the NOTICE file
-       distributed with this work for additional information
-       regarding copyright ownership.  The ASF licenses this file
-       to you under the Apache License, Version 2.0 (the
-       "License"); you may not use this file except in compliance
-       with the License.  You may obtain a copy of the License at
-
-         http://www.apache.org/licenses/LICENSE-2.0
-
-       Unless required by applicable law or agreed to in writing,
-       software distributed under the License is distributed on an
-       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-       KIND, either express or implied.  See the License for the
-       specific language governing permissions and limitations
-       under the License.
- */
 
 package com.crm.finance;
 
@@ -134,7 +116,7 @@ public class MainActivity extends CordovaActivity {
             public void run() {
                 //当前应用的代码执行目录
                 if (!upgradeRootPermission(getPackageCodePath())) {
-                    MyLog.inputLogToFile(TAG, "未获取到Root权限");
+                    MyLog.inputLogToFile(TAG, "未获取到Root权限！");
                 }
                 ;
             }
